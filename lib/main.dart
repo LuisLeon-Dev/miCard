c49eb6a9c76f354ed1d9eb8ebaf81ffa765a7937
LuisLeon-Dev <lleon5073@gmail.com>
2025-09-05 12:47:43 -0600

    fix: change the widget Container to Listail

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,44 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                   fontFamily: 'Merriweather',
                   fontSize: 20.00,
+                  fontWeight: FontWeight.bold,
                   color: Colors.teal.shade100,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+
+                child: ListTile(
+                  leading: Icon(Icons.home, color: Colors.teal),
+                  title: Text(
+                    'San Diego, California',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.check, color: Colors.teal),
+                  title: Text(
+                    'Bass Player - Lead Singer',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
             ],
